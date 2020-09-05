b7eb2d7bb3c15cdc90a67f2badb0c217e1488752
Hiroyuki ONODA <hiro.onoda@gmail.com>
2020-09-05 13:53:53 +0900

    update: switch environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
+const env = process.env.NODE_ENV || 'production'
 
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: 'production',
+  mode: env,
   entry: './src/index.jsx',
   output: {
     path: __dirname + '/public',
