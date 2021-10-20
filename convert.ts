76790e8873627f7dc4f884129d66b9c89cf0e8ae
Hiroyuki ONODA <hiro.onoda@gmail.com>
2021-10-20 14:56:41 +0900

    added convert script

---
+++ convert.ts
@@ -0,0 +1,47 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import { v4 as uuidv4 } from 'uuid'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA8NGwQmCq5TF16lQy4fSAyti8zpHIwXqI',
+  authDomain: 'dnd35-character-database.firebaseapp.com',
+  databaseURL: 'https://dnd35-character-database.firebaseio.com',
+  projectId: 'dnd35-character-database',
+  storageBucket: 'dnd35-character-database.appspot.com',
+  messagingSenderId: '551444495572',
+  appId: '1:551444495572:web:5ef6070eb157db6da50f41',
+  measurementId: 'G-0M7NMCXK1Y',
+}
+
+firebase.initializeApp(firebaseConfig)
+firebase.firestore().useEmulator('localhost', 8081)
+const db = firebase.firestore()
+const batch = db.batch();
+
+const main = async () => {
+  const snapshots = await db.collection('characters').get()
+  snapshots.docs.map((doc) => {
+    Object.entries(doc.data()).map(([key, value]) => {
+      if (Array.isArray(value)) {
+        value.map((item)=> {
+          item['id'] = uuidv4()
+        })
+        const updateData = {}
+        updateData[key] = value
+        console.log(updateData)
+        batch.update(doc.ref, updateData)
+      } else if (value !== null && typeof value === 'object') {
+        value['id'] = uuidv4()
+        const updateData = {}
+        updateData[key] = value
+        console.log(updateData)
+        batch.update(doc.ref, updateData)
+      }
+    })
+  })
+  console.log('updated')
+  await batch.commit()
+  console.log('commited')
+}
+
+main()
